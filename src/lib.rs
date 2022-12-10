f033e75291c299f212042fe2cbc1d147cdeba344
Will Zegers <will.zegers@gmail.com>
2022-12-10 21:25:10 +0000

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -48,7 +48,7 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width*height)
+        let cells = (0..width * height)
             .map(|i| {
                 if i % 2 == 0 || i % 7 == 0 {
                     Cell::Alive
